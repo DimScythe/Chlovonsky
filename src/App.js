81eb3d8854630df2f46c6a5865c7f251ba4c7a48
DimScythe <darkblitz798@gmail.com>
2021-06-10 12:26:56 +0300

    Salwa is a great instructor

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import ShopImage from "./ShopImage.png"
 import './App.css';
+import cookies from "./cookies"
 
 function App() {
+  const cookieList = cookies.map((cookie) =>{
+    return(
+      <div className="cookie-container" key={cookie.id}>
+        <img src={cookie.image} alt={cookie.name} className="cookie-image"/>
+        <p>{cookie.name}</p>
+        <p>{cookie.price} KWD</p>
+      </div>
+    );
+  });
   return (
   <div>
-    <h1>Chlovonsky</h1>
-    <h2>How I got the recipe:</h2>
-    <p>One day I was watching a British Twtich Streamer
-      and he was baking some cookies and I asked for the
-      recipe and I got it from him and I started selling it
-      :D
-    </p>
-    <img src={ShopImage} alt="Cookie Shop front store" class="center"
-          height={350}
-          width={350}/>
+    <div>
+      <h1>Chlovonsky</h1>
+      <h2>How I got the recipe:</h2>
+      <p>One day I was watching a British Twitch Streamer
+         and he was baking some cookies and I asked for the
+         recipe and I got it from him and I started selling it
+         :D
+      </p>
+      <img 
+        src={ShopImage} alt="Cookie Shop front store" class="center"
+        height={350}
+        width={350}/>
     </div>
+    <div className="list-wrapper">{cookieList}</div>
+  </div>
   );
 }
 
